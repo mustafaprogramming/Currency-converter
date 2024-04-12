947972a5102f69b263100498e266b7c455150c29
mustafaghaffar763@gmail.com <mustafaghaffar763@gmail.com>
2024-04-12 15:23:21 +0500

    Adding Currency converter

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,42 @@
 import inquirer from "inquirer";
 import chalk from 'chalk';
 
-console.log(chalk.whiteBright.bold(`<<<==========================================================================>>>`));
+console.log(chalk.whiteBright.bold(`\n<<<==========================================================================>>>`));
 console.log(chalk.blueBright.bold(`\n\t\tWelcome To Mustafa - Currency Converter\t\t`));
-console.log(chalk.whiteBright.bold(`<<<==========================================================================>>>`));
+console.log(chalk.whiteBright.bold(`<<<==========================================================================>>>\n`));
+
+const exchange_rates:{[key:string]:number}={
+    "EUR": 0.94,
+    "CAD":1.37,
+    "JYP":153.29,
+    "PKR":278.07,
+    "AUD":1.54,
+    "USD": 1,
+}
+
+let user_answer=await inquirer.prompt([
+    {
+        name:'from_currency',
+        type:'list',
+        message:chalk.rgb(255, 140, 0).bold('Select the currency to convert from: '),
+        choices:["EUR","CAD","JYP","PKR","AUD","USD",]
+    },
+    {
+        type:"number",
+        name:'amount',
+        message:chalk.rgb(255, 140, 0).bold('Enter the amount to Convert: '),
+    },
+    {
+        name:'to_currency',
+        type:'list',
+        message:chalk.rgb(255, 140, 0).bold('Select the currency to convert into: '),
+        choices:["EUR","CAD","JYP","PKR","AUD","USD",]
+    }
+]);
+ let from_amount=exchange_rates[user_answer.from_currency];
+ let to_amount=exchange_rates[user_answer.to_currency];
+ let conversion_rate=to_amount/from_amount;
+ console.log(chalk.greenBright.bold(`\nConverted Amount: ${chalk.yellowBright.bold((user_answer.amount*conversion_rate).toFixed(2))}`));
+
+
 
